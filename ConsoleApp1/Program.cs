5004cdfcc362f8e7548e6af38cc504f19e8ff534
devlopersabbir <devlopersabbir@gmail.com>
2023-05-09 20:22:10 +0600

    Age calculator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,21 @@ namespace NameSpace
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your dath of birth: ");
+            string userInput = Console.ReadLine();
+            // Console.Write(userInput);
+
+            if (string.IsNullOrEmpty(userInput))
+            {
+                Console.Write("Input filed is empty!");
+            }else
+            {
+                int userDateOfBirth = Convert.ToInt32(userInput ?? userInput);
+                const int currentDate = 2023;
+            int age = currentDate - userDateOfBirth;
+            Console.Write($"Your age is {age}");
             
+            }
         }
     }
 }
