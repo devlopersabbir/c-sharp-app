ee8658aa7b2e436494e4e3c6013690468e1151b3
devlopersabbir <devlopersabbir@gmail.com>
2023-05-09 19:50:45 +0600

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NotirPula
+{
+    class OurProgramMagi
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your username: ");
+            string username = Console.ReadLine();
+            Console.Write("Username is: " + username);
+        }
+    }
+}
